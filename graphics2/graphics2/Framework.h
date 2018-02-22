8679fafe935caa7f5546310e6a070ddcf395aae1
Bazzadwarf <bazzadobs@live.co.uk>
2018-02-22 01:24:18 +0000

    aaaaaaaaaaa

---
+++ graphics2/graphics2/Framework.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include <exception>
+#include <memory>
+
+#define DEFAULT_FRAMERATE 60
+#define DEFAULT_WIDTH 640
+#define DEFAULT_HEIGHT 480
+
+class Framework
+{
+public:
+	Framework();
+	~Framework();
+};
+
