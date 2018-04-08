d14a0dca8728e05e6084eff0ba0944ffc59acc1f
Bazzadwarf <bazzadobs@live.co.uk>
2018-04-08 21:09:40 +0100

    fgdhjn
    dfgh

--- GraphicsRenderer/GraphicsRenderer/Graphics.cpp
+++ GraphicsRenderer/GraphicsRenderer/Graphics.cpp
@@ -19,5 +19,5 @@ void Graphics::UpdateSceneGraph()
 	SceneGraphPointer sceneGraph = GetSceneGraph();
 	_rotationAngle += 1.0f;
 	//sceneGraph->Find(L"cube")->SetLocalTransform(XMMatrixRotationAxis(XMVectorSet(1.0f, 1.0f, 1.0f, 0.0f), _rotationAngle * 0.5f * XM_PI / 180.0f));
-	//sceneGraph->Find(L"Plane1")->Update(XMMatrixTranslation(0, 0, _rotationAngle));
+	sceneGraph->Find(L"Plane1")->Update(XMMatrixTranslation(0, 0, _rotationAngle));
 }
